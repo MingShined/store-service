21ec95d6e7023c99e903fc37e3126353f9d56032
mingshined <996578843@qq.com>
2019-03-20 22:42:49 +0800

    fix 购物车bug

--- app/service/shopcart.js
+++ app/service/shopcart.js
@@ -25,7 +25,7 @@ class ShopcartService extends Service {
   }
   async create(payload) {
     const { ctx } = this;
-    const { goodId, size, quantity } = payload;
+    const { id, size, quantity } = payload;
 
     /**
      * @name ===========未登录==============
@@ -44,8 +44,8 @@ class ShopcartService extends Service {
      * @name ===已经添加过相同商品，数量加1===
      */
     const shopcartInfo = await ctx.service.shopcart.findOne({
-      userId,
-      'goodInfo._id': goodId,
+      'userInfo._id': userId,
+      'goodInfo._id': id,
       'goodInfo.size': size,
     });
     // 若存在，累加数量
@@ -63,7 +63,7 @@ class ShopcartService extends Service {
      */
     let userInfo = await ctx.service.user.findOne({ _id: userId });
     userInfo = JSON.parse(JSON.stringify(userInfo));
-    let goodInfo = await ctx.service.good.findOne({ _id: goodId });
+    let goodInfo = await ctx.service.good.findOne({ _id: id });
     goodInfo = JSON.parse(JSON.stringify(goodInfo));
     goodInfo = { ...goodInfo, size, quantity: quantity ? quantity : 1 };
     const result = { userInfo, goodInfo };
